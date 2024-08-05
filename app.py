640e960bec01aaee2a85847c4318a236205ccc7e
Abdulrehman Moaz <143192960+abdul-34@users.noreply.github.com>
2024-08-05 11:47:55 +0000

    Added Dynamic Templates and API routes

--- app.py
+++ app.py
@@ -1,11 +1,50 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS=[
+  {
+    "id": 1,
+    "title": "Software Engineer",
+    "location": "San Francisco, CA",
+    "salary": "120000"
+  },
+  {
+    "id": 2,
+    "title": "Data Scientist",
+    "location": "New York, NY",
+    "salary": "110000"
+  },
+  {
+    "id": 3,
+    "title": "Product Manager",
+    "location": "Austin, TX",
+    "salary": "115000"
+  },
+  {
+    "id": 4,
+    "title": "UX Designer",
+    "location": "Seattle, WA",
+    "salary": "105000"
+  },
+  {
+    "id": 5,
+    "title": "DevOps Engineer",
+    "location": "Chicago, IL",
+    "salary": "118000"
+  }
+]
+
+
 
 @app.route('/')
 
 def hello_world():
-    return 'Hello, World!'
+    return render_template('home.html',jobs=JOBS)
 
+@app.route('/api/jobs')
+
+def list_jobs():
+    return jsonify(JOBS)
 if __name__ == '__main__':
    app.run(host='0.0.0.0',debug=True)
